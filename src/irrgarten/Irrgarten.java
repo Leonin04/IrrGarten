e92950dc0d3b1423ac775ec64149b2ab336f78db
Leonin04 <davidbacasposadas@gmail.com>
2025-09-24 16:05:55 +0200

    primer push

---
+++ src/irrgarten/Irrgarten.java
@@ -0,0 +1,16 @@
+package irrgarten;
+
+/**
+ *
+ * @author leonin04
+ */
+public class Irrgarten {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("hola");
+    }
+    
+}
