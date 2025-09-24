bb426b51a938e66c540f2462107eb5de24c2cfe4
Leonin04 <davidbacasposadas@gmail.com>
2025-09-24 16:12:05 +0200

    Primer push

--- src/irrgarten/Weapon.java
+++ src/irrgarten/Weapon.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package irrgarten;
 
 /**
@@ -9,5 +6,12 @@ package irrgarten;
  * @author leonin04
  */
 public class Weapon {
+    private float power;
+    private int uses;
+    
+    public Weapon (float power, int uses) {
+        this.power = power;
+        this.uses = uses;
+    }
     
 }
