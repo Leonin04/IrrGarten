e5fc7fab9f187ac67bd659d7367d1e9260d0deeb
Leonin04 <davidbacasposadas@gmail.com>
2025-09-29 19:20:21 +0200

    Clase shield hecha en java y rubi

--- Irrgarten/src/irrgarten/Irrgarten.java
+++ Irrgarten/src/irrgarten/Irrgarten.java
@@ -1,22 +1,41 @@
 package irrgarten;
 
-/**
- *
- * @author leonin04
- */
+enum Directions {
+    LEFT, RIGHT, UP, DOWN
+}
+
+enum Orientation {
+    VERTICAL, HORIZONTAL
+}
+
+enum GameCharacter {
+    PLAYER, MONSTER
+}
 public class Irrgarten {
 
-    static void pruebaWeapon (){
-        Weapon arma1 = new Weapon(10.0f,1);
-        arma1.attack();
+    static void pruebaWeapon (float daño, int usos){
+        Weapon arma1 = new Weapon(daño,usos);
+        System.out.println("Dañó: " + arma1.attack());
         
         System.out.println(arma1.toString());
     }
+    
+    static void pruebaShield(float durabilidad, int usos) {
+        Shield escudo1 = new Shield(durabilidad,usos);
+        System.out.println("Protegio: " + escudo1.protect());
+        
+        System.out.println(escudo1.toString());
+    }
             
     public static void main(String[] args) {
-       System.out.println("hola");
-       pruebaWeapon();
-       pruebaWeapon();
+       System.out.println("Probando arma:");
+       pruebaWeapon(10.0f,1);
+       pruebaWeapon(10.0f,0);
+       
+       System.out.println("\n Probando escudo:");
+       pruebaShield(10.0f,1);
+       pruebaShield(10.0f,0);
+
     }
     
 }
--- Irrgarten_Ruby/Irrgarten.rb
+++ Irrgarten_Ruby/Irrgarten.rb
@@ -1,6 +1,24 @@
 #encoding:utf-8
 
 require_relative 'Weapon'
+require_relative 'Shield'
+
+module Directions
+	LEFT =:left
+	RIGHT=:right
+	UP=:up
+	DOWN=:down
+end
+
+module Orientation
+	VERTICAL=:vertical
+	HORIZONTAL=:horizontal
+end
+
+module GameCharacter
+	PLAYER =:player
+	MONSTER =:monster
+end
 
 module Irrgarten
 	
@@ -8,10 +26,21 @@ module Irrgarten
 		
 		def self.prueba_weapon
 			w = Weapon.new(2.0, 3)
+			puts "\n"
 			puts w.to_s	
 			4.times do
 				puts "Potencia de disparo #{w.attack}"
 				puts w.to_s
+			end
+		end
+		
+		def self.prueba_shield
+			s = Shield.new(2.0,3)
+			puts "\n"
+			puts s.to_s
+			4.times do
+				puts "Daño protegido #{s.protect}"
+				puts s.to_s
 			end
 		end
 		
@@ -22,3 +51,4 @@ module Irrgarten
 end
 
 Irrgarten::Irrgarten.prueba_weapon
+Irrgarten::Irrgarten.prueba_shield
