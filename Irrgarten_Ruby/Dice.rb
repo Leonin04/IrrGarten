adb9fb1a347ec5fed1d87630f05fec178249304f
mine1712 <134804710+mine1712@users.noreply.github.com>
2025-10-03 09:52:58 +0200

    Poner self y @@ en Dice.rb

--- Irrgarten_Ruby/Dice.rb
+++ Irrgarten_Ruby/Dice.rb
@@ -15,75 +15,75 @@ module Irrgarten
 			
 		@@generator = Random.new
 		
-		def random_pos ( max ) #int
-			@generator.rand(0...max) # ... excluido .. incluido
+		def self.random_pos ( max ) #int
+			@@generator.rand(0...max) # ... excluido .. incluido
 		 
 		end
 		
 		
 		def who_starts ( nplayers ) #int
-			@generator.rand(0...nplayers)
+			@@generator.rand(0...nplayers)
 		
 		end
 		
 		
-		def random_intelligence()
-			@generator.rand(0...@MAX_INTELLIGENCE)
+		def self.random_intelligence()
+			@@generator.rand(0...@@MAX_INTELLIGENCE)
 		end
 		
 		
-		def random_strength()
-			@generator.rand(0...@MAX_STRENGHT)
+		def self.random_strength()
+			@@generator.rand(0...@@MAX_STRENGHT)
 		end
 		
 		
-		def resurrect_player()
-			if (@generador.rand < 0.3)
+		def self.resurrect_player()
+			if (@@generador.rand < 0.3)
 				return true
 			else
 				return false
 		end
 		
 		
-		def weapons_reward()
-			@generator.rand(0..@WEAPONS_REWARD)
+		def self.weapons_reward()
+			@@generator.rand(0..@@WEAPONS_REWARD)
 		end
 		
 		
-		def shields_reward()
-			@generator.rand(0..@SHIELDS_REWARD)
+		def self.shields_reward()
+			@@generator.rand(0..@@SHIELDS_REWARD)
 		end
 		
 		
-		def health_reward()
-			@generator.rand(0..@HEALTH_REWARD)
+		def self.health_reward()
+			@@generator.rand(0..@@HEALTH_REWARD)
 		end
 		
 		
-		def weapon_power()
-			@generator.rand(0...@MAX_ATTACK)
+		def self.weapon_power()
+			@@generator.rand(0...@@MAX_ATTACK)
 		end
 		
 		
-		def shield_power()
-			@generator.rand(0...@MAX_SHIELD)
+		def self.shield_power()
+			@@generator.rand(0...@@MAX_SHIELD)
 		end
 		
 		
-		def uses_left()
-			@generator.rand(0..@MAX_USES)
+		def self.uses_left()
+			@@generator.rand(0..@@MAX_USES)
 		end
 		
 		
-		def intensity( competence )
-			@generator.rand(0...competence)
+		def selfintensity( competence )
+			@@generator.rand(0...competence)
 		end
 		
 		
-		def discard_element( uses_left )
+		def self.discard_element( uses_left )
 			descartado = false
-			probabilidad = 1.0 -  (uses_left.to_f/@MAX_USES.to_f)
-			if @generator.rand(0.0..1.0) < probabilidad
+			probabilidad = 1.0 -  (uses_left.to_f/@@MAX_USES.to_f)
+			if @@generator.rand(0.0..1.0) < probabilidad
 			    descartado = true
 			end
 			
